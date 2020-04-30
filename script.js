93aacff5d0a1fb41aa0f04490d20fa65659d8944
Sarah Vivoli <snvivoli@gmail.com>
2020-04-30 11:33:36 -0700

    removed banner from sign up page

--- script.js
+++ script.js
@@ -1,13 +1,15 @@
 const privacy = $(".privacy");
 const next1 = $(".next1");
 const agree = $(".agree");
+const stolen = $(".stolen");
 
 
-
-next1.on("click", function() {
+next1.on("click", function(event) {
+    event.preventDefault();
     if (!agree.is(':checked')) {
         alert("You must agree to the Terms of Service & Privacy Policy to proceed.");
     } else {
         privacy.hide();
+        stolen.show();
     }
 })
