fca2a7b09dca4a569b70be15861b3c12f04582f7
Sarah Vivoli <snvivoli@gmail.com>
2020-05-01 11:52:52 -0700

    nan if statement for est value input

--- script.js
+++ script.js
@@ -1,44 +1,51 @@
-const about = $(".about");
-const next1 = $(".next1");
-const agree = $(".agree");
-const stolen = $(".stolen");
-const up = $(".up");
-const down = $(".down");
-const next2 = $(".next2");
-const type = $(".type");
-const next3 = $(".next3");
-const you = $(".you");
+const about = $('.about');
+const value = $('.value');
+const next1 = $('.next1');
+const agree = $('.agree');
+const often = $('.often');
+const up = $('.up');
+const down = $('.down');
+const next2 = $('.next2');
+const type = $('.type');
+const next3 = $('.next3');
+const you = $('.you');
 
 
-next1.on("click", function(event) {
+
+next1.on('click', function(event) {
+    console.log(isNaN(value.val()));
     event.preventDefault();
+    if (isNaN(value.val())) {
+        alert('Estimated value must be a numerical value.')
+    }
     if (!agree.is(':checked')) {
-        alert("You must agree to the Terms of Service & Privacy Policy to proceed.");
-    } else {
+        alert('You must agree to the Terms of Service & Privacy Policy to proceed.');
+    }
+    if (!isNaN(value.val()) && agree.is(':checked')) {
         about.hide();
-        stolen.show();
+        often.show();
     }
 });
 
-$('.up, .down').on("click", function(event) {
+$('.up, .down').on('click', function(event) {
     event.preventDefault();
     $('.active').removeClass('active');
     $(this).addClass('active');
 });
 
-next2.on("click", function(event) {
+next2.on('click', function(event) {
     event.preventDefault();
-    stolen.hide();
+    often.hide();
     type.show();
 });
 
-next2.on("click", function(event) {
+next2.on('click', function(event) {
     event.preventDefault();
     type.hide();
     you.show();
 });
 
-$('.light', '.med', '.heavy').on("click", function(event) {
+$('.light', '.med', '.heavy').on('click', function(event) {
     event.preventDefault();
     $('.active1').removeClass('active1');
     $(this).addClass('active1');
