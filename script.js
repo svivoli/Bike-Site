6ab8fba45258b616e6ad602b5a8d0004fd76a424
Sarah Vivoli <snvivoli@gmail.com>
2020-05-01 10:16:31 -0700

    new signup page 1

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-const privacy = $(".privacy");
+const about = $(".about");
 const next1 = $(".next1");
 const agree = $(".agree");
 const stolen = $(".stolen");
@@ -6,6 +6,8 @@ const up = $(".up");
 const down = $(".down");
 const next2 = $(".next2");
 const type = $(".type");
+const next3 = $(".next3");
+const you = $(".you");
 
 
 next1.on("click", function(event) {
@@ -13,7 +15,7 @@ next1.on("click", function(event) {
     if (!agree.is(':checked')) {
         alert("You must agree to the Terms of Service & Privacy Policy to proceed.");
     } else {
-        privacy.hide();
+        about.hide();
         stolen.show();
     }
 });
@@ -28,4 +30,16 @@ next2.on("click", function(event) {
     event.preventDefault();
     stolen.hide();
     type.show();
+});
+
+next2.on("click", function(event) {
+    event.preventDefault();
+    type.hide();
+    you.show();
+});
+
+$('.light', '.med', '.heavy').on("click", function(event) {
+    event.preventDefault();
+    $('.active1').removeClass('active1');
+    $(this).addClass('active1');
 })
