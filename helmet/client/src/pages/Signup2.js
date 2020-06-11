8d62c0268e22a3dbd0cc2c90a36d58321d538d0a
Sarah Vivoli <snvivoli@gmail.com>
2020-06-11 11:29:33 -0700

    signup2 page

---
+++ helmet/client/src/pages/Signup2.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import Navbar from '../components/Navbar';
+import SignUpSection from '../components/SignUpSection';
+import Often from '../components/Often';
+
+function SignUp2() {
+    return(
+        <div>
+        <Navbar />
+        <SignUpSection>
+            <Often />
+        </SignUpSection>
+        </div>
+    )
+};
+
+export default SignUp2;
