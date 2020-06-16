6413ffd2d56b21bf37848d862c9f317a18b2ea8f
Sarah Vivoli <snvivoli@gmail.com>
2020-06-16 15:32:34 -0700

    alert for selection requirement on frequency page

--- helmet/client/src/components/Often/index.js
+++ helmet/client/src/components/Often/index.js
@@ -9,11 +9,17 @@ function Often() {
     }
 
     function handlePost() {
-        API.createFrequency({
-            often: often
-        })
-            .then(console.log("Frequency saved to database."))
-            .catch(err => console.log(err));
+        if (often == undefined) {
+            alert('Please make a selection');
+        }
+        else {
+            API.createFrequency({
+                often: often
+            })
+                .then(console.log("Frequency saved to database."))
+                .catch(err => console.log(err))
+                .then(document.location.href = '/signup3');
+        }
     }
 
     return (
@@ -25,7 +31,7 @@ function Often() {
                     <input type="button" className="med uk-button uk-button-default" value="10+ Days/Month" onClick={handleOftenChange}></input>
                     <input type="button" className="heavy uk-button uk-button-default" value="Daily" onClick={handleOftenChange}></input>
                     <br></br>
-                    <a className="next2-btn uk-button uk-button-default" href="/signup3" onClick={handlePost}>Next</a>
+                    <a className="next2-btn uk-button uk-button-default" href="#" onClick={handlePost}>Next</a>
                 </div>
             </div>
         </section>
