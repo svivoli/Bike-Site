748db3cc8e25df103218ee6ae15ba8fdcfb5f820
Sarah Vivoli <snvivoli@gmail.com>
2020-06-16 15:44:18 -0700

    heroku deploy

---
+++ helmet/client/build/precache-manifest.f23d3d4cfc343ed40934296b73776cfa.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b6aaf4b35eb3cf50c6157dbe4bbd3c0a",
+    "url": "/index.html"
+  },
+  {
+    "revision": "3016b3f6985c2984f1b9",
+    "url": "/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "27880af367a025a3b7c9",
+    "url": "/static/js/2.cba60a9b.chunk.js"
+  },
+  {
+    "revision": "aef280276b8d6f56e9d8dc46988119d9",
+    "url": "/static/js/2.cba60a9b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3016b3f6985c2984f1b9",
+    "url": "/static/js/main.da8a1780.chunk.js"
+  },
+  {
+    "revision": "3c59cb62b38aa7b4fbd7",
+    "url": "/static/js/runtime-main.44c5fd8f.js"
+  }
+]);
